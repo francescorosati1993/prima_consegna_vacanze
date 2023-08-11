e884caf746ecfe22697c21c68e6ffdb646d6e293
francescorosati1993 <rosati.francesco1993@gmail.com>
2023-08-11 20:05:06 +0200

    Versione 2.0 - Esercizio Completato

--- Gestore_Aziendale/src/com/generation/controller/ChiusuraTicketAperti.java
+++ Gestore_Aziendale/src/com/generation/controller/ChiusuraTicketAperti.java
@@ -7,7 +7,6 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.generation.entities.Company;
 import com.generation.entities.Contact;
 import com.generation.entities.Ticket;
 import com.generation.library.Console;
@@ -29,23 +28,6 @@ public class ChiusuraTicketAperti {
 		{
 			Console.print("Connessione al database non riuscita, controllare il file config.txt");
 		}
-		
-//		Console.print("Inserisci settore di cui vuoi controllare i ticket aperti");
-//		String sector = Console.readString();	
-//
-//		Company company = new Company();
-//		
-//		try 
-//		{
-//			if(db.findCompanyBySector(sector).size() == 1)
-//				company = db.findCompanyBySector(sector).get(0);
-//		} 
-//		catch (SQLException e) 
-//		{
-//			Console.print("Connessione al database non riuscita, controllare il file config.txt");
-//		}
-		
-		
 		
 		
 		List<Contact> contacts = new ArrayList<Contact>();
@@ -70,10 +52,7 @@ public class ChiusuraTicketAperti {
 			Console.print("Connessione al database non riuscita, controllare il file config.txt");
 		}
 		
-		
-	//	List<Ticket> updateTickets = new ArrayList<Ticket>();
-		
-		
+	
 		for(Contact contact : contacts) 
 				for(Ticket ticket : tickets)
 					if(ticket.getContact_id() == contact.getId() && ticket.getClosedOn()==null)
@@ -92,27 +71,6 @@ public class ChiusuraTicketAperti {
 							
 						}
 		
-
-//							
-//		
-//		Ticket updateTicket = new Ticket();
-//		
-//		for(int i = 0; i<contacts.size(); i++)
-//			for(int j = 0; j<tickets.size(); j++)
-//				if(tickets.get(j).getContact_id() == contacts.get(i).getId() && tickets.get(j).getClosedOn()==null)
-//					if(ChronoUnit.DAYS.between(tickets.get(j).getSentOn(), LocalDateTime.now())>=31)
-//						{
-//							Console.print(tickets.get(j));
-//							updateTicket.setId(tickets.get(j).getId());
-//							updateTicket.setContact_id(tickets.get(j).getContact_id());
-//							updateTicket.setRequest(tickets.get(j).getRequest());
-//							updateTicket.setResponse("Grave ritardo nella gestione, si prega di creare un nuovo ticket");
-//							updateTicket.setSentOn(tickets.get(j).getSentOn());
-//							updateTicket.setClosedOn(LocalDateTime.now());
-//						
-//							db.updateTicket(updateTicket);
-//						}
-//		
 	}
 
 }
---
+++ Gestore_Aziendale/src/com/generation/controller/InserimentoContattiDaCSV.java
@@ -0,0 +1,64 @@
+package com.generation.controller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import com.generation.entities.Contact;
+import com.generation.library.Console;
+import com.generation.repository.Database;
+
+public class InserimentoContattiDaCSV {
+	
+	private static Database db;
+	
+	public static void main(String[] args) throws SQLException 
+	{
+		
+		try 
+		{
+			db = new Database("config.txt","ticket","contact","company");
+		} 
+		catch (FileNotFoundException | SQLException e) 
+		{
+			Console.print("Connessione al database non riuscita, controllare il file config.txt");
+		}
+		
+		
+		Console.print("Inserisci il nome del file .csv da leggere: ");
+		String fileName = Console.readString();
+		
+		File f = new File(fileName);
+		Scanner reader = null;
+		
+		try 
+		{
+			reader = new Scanner(f);
+		} 
+		catch (FileNotFoundException e) 
+		{
+			Console.print("File non trovato, inserire nome file corretto e riprovare");
+		}
+		
+		
+		while(reader.hasNextLine()) 
+		{
+			Contact contact = new Contact();
+			
+			String contactCSV = reader.nextLine();
+			String[] column = contactCSV.split(";");
+		
+			contact.setId(Integer.parseInt(column[0]));
+			contact.setCompany_id(Integer.parseInt(column[1]));
+			contact.setName(column[2]);
+			contact.setSurname(column[3]);
+			contact.setAddress(column[4]);
+			contact.setCity(column[5]);
+		
+			db.insertContact(contact);
+		}
+	
+	}
+
+}
